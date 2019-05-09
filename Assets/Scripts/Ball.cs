7e315b8cf85e4063843ded5ef912f01dace0d98b
Sergi <meuxilus@gmail.com>
2019-05-09 20:12:20 +0200

    new bouncing system, needs improvement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Ball : MonoBehaviour
 {
     public float speed;
-
     public bool isLeftTo2Right;
 
     private Vector2 direction;
@@ -21,7 +21,7 @@ public class Ball : MonoBehaviour
         {
             direction = Vector2.left;
         }
-        direction.y += Random.Range(-0.5f, 0.5f) > 0? 0.5f:-0.5f;
+        direction.y += UnityEngine.Random.Range(-0.5f, 0.5f) > 0 ? 0.5f : -0.5f;
     }
 
     private void Update()
@@ -32,19 +32,63 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Ball otherBall = other.GetComponent<Ball>();
+
         if (isLeftTo2Right && other.name.Equals("RightWall"))
         {
             Destroy(gameObject);
 
         }
+
         if (!isLeftTo2Right && other.name.Equals("LeftWall"))
         {
             Destroy(gameObject);
         }
-        if (other.name.Equals("TopWall") || other.name.Equals("BottomWall") || other.name.StartsWith("Ball"))
+        if (other.name.Equals("TopWall") || other.name.Equals("BottomWall"))
         {
             direction.y *= -1;
+        }
+        if (other.name.StartsWith("Ball"))
+        {
+            if (transform.position.y <= other.transform.position.y + 0.45
+                && transform.position.y >= other.transform.position.y - 0.45)
+            {
+                direction.x *= -1;
+            } else if (transform.position.x <= other.transform.position.x + 0.45
+                && transform.position.x >= other.transform.position.x - 0.45)
+            {
+                direction.y *= -1;
 
+            }
+            // else if (Math.Sign(otherBall.direction.y) != Math.Sign(direction.y)
+            //     && Math.Sign(otherBall.direction.x) != Math.Sign(direction.x))
+            // {
+            //     direction.x *= -1;
+            //     direction.y *= -1;
+            // }
+            // else if (Math.Sign(otherBall.direction.y) == Math.Sign(direction.y)
+            //     && Math.Sign(otherBall.direction.x) != Math.Sign(direction.x))
+            // {
+            //     direction.x *= -1;
+            // }
+            // else if (Math.Sign(otherBall.direction.y) == Math.Sign(direction.y)
+            //     && Math.Sign(otherBall.direction.x) == Math.Sign(direction.x))
+            // {
+            //     if (transform.position.y < other.transform.position.y)
+            //     {
+            //         direction.y *= -1;
+            //     }
+            //     // else if (otherBall.speed > speed)
+            //     // {
+            //     //     direction.y *= -1;
+            //     // }
+
+            // }
+            // else if (Math.Sign(otherBall.direction.y) != Math.Sign(direction.y)
+            //     && Math.Sign(otherBall.direction.x) == Math.Sign(direction.x))
+            // {
+            //     direction.x *= -1;
+            // }
         }
     }
 }
